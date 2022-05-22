7c5679893f54eeee91c231d962f961a1ecdabb0e
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-22 06:08:32 +0600

    deployed completed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log("The Gigawatt is running!")
+        console.log("The Gigawatt Deployed!")
     } finally {
 
     }
