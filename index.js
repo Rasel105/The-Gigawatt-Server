ff78c972c1d19a7c76102e288e89ac9b1a50bea2
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-23 20:35:36 +0600

    purchase api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require("cors")
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // app is called instane 
 const port = process.env.PORT || 5000;
 
@@ -17,21 +17,33 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db("gigawatt").collection("products");
+        const purchaseCollection = client.db("gigawatt").collection("purchase");
 
         app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        })
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.post("/purchase", async (req, res) => {
+            const newPurchase = req.body;
+            const result = await purchaseCollection.insertOne(newPurchase);
+            res.send(result);
+        });
+
     } finally {
 
     }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", async (req, res) => {
     res.send("I am ready");
