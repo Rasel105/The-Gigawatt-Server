4e1cf699f1b2f20f72b1d8dbd0de86cebaab7cd3
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-05-24 07:21:08 +0600

    users get api created

--- index.js
+++ index.js
@@ -14,12 +14,35 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.v59jk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Fobidden" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run() {
     try {
         await client.connect();
         const productCollection = client.db("gigawatt").collection("products");
         const purchaseCollection = client.db("gigawatt").collection("purchase");
         const userCollection = client.db("gigawatt").collection("users");
+
+
+        app.get('/users', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
 
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
@@ -32,7 +55,7 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
             res.send({ result, accessToken: token });
-        })
+        });
 
 
         app.get('/products', async (req, res) => {
@@ -54,6 +77,23 @@ async function run() {
             const result = await purchaseCollection.insertOne(newPurchase);
             res.send(result);
         });
+
+
+        app.get("/myorders", verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            // const authorization = req.headers.authorization;
+            // console.log("Authheder", authorization);
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = purchaseCollection.find(query);
+                const myorders = await cursor.toArray();
+                return res.send(myorders);
+            }
+            else {
+                return res.status(403).send({ message: "Forbidden access" });
+            }
+        })
 
         // app.get('/myorders', async (req, res) => {
         //     // const decodedEmail = req.decoded.email;
